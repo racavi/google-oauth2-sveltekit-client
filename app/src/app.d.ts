5b0c1cfe43614bfd7d93f8d0ae5ba5cd14907702
racavi <63754467+racavi@users.noreply.github.com>
2023-09-26 18:52:49 +0200

    Extend Locals interface to include a custom Session type

--- app/src/app.d.ts
+++ app/src/app.d.ts
@@ -1,9 +1,16 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+type Session = {
+	credentials: string | null
+}
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			session: Session | null
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
